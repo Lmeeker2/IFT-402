fe4564779569a60681b87348a15baa06e68a2a8f
gitnhubed <nbdodson1@gmail.com>
2023-03-18 16:21:56 -0400

    Update login.js

--- login.js
+++ login.js
@@ -2,9 +2,11 @@ function validate()
 {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-    if(username=="admin" && password=="user")
+    if((username=="admin" && password=="user") || (username=="blake" && password=="blake") || (username=="lauren" && password=="lauren") 
+    || (username=="jake" && password=="jake") || (username=="daniel" && password=="daniel"))
     {
-        alert("login succesfully");
+        //alert("login succesfully");
+        window.location.replace("home.html");
         return false;
     }
     else
